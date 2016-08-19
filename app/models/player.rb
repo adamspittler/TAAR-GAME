4aa15ef1b24fd64fe712bbeee8dbdfc030ff31db
Richie Ito <email": null>
2016-08-19 10:54:08 -0400

    add migrations and models

--- app/models/player.rb
+++ app/models/player.rb
@@ -5,4 +5,10 @@ class Player < ActiveRecord::Base
   has_many :rounds, through: :player_rounds
   has_many :player_abilities
   has_many :abilities, through: :player_abilities
+
+  after_initialize: :set_defaults
+
+  def set_defaults
+    self.coins = 4
+  end
 end
